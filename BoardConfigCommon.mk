6574c7b9b1c7a67685a18e8d31a5a3c19a581b27
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-10-24 14:02:53 +0700

    sm8450-common: Use RSA2048 algorithm and key for vbmeta
    vbmeta on stock uses RSA2048 algorithm but current vbmeta uses RSA4096
    algorithm, so override BOARD_AVB_ALGORITHM and BOARD_AVB_KEY_PATH to
    use RSA2048 algorithm and key to align with stock.
    
    Change-Id: I774b0a058c8190ba04dd14e25916c5d9fbb28a5f
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -218,6 +218,8 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/vintf/framework_manifest.xml
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
+BOARD_AVB_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
